aad9bf9e85dc878e5e1702c687d348bddd841f47
examp1 <bebe4kaxd@gmail.com>
2021-03-06 16:32:36 +0200

    123

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,14 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
-   
-    <v-btn
-      class="pink white--text"
-    >click me</v-btn>
-    <v-btn
-      depressed
-      color="pink"
-    >click me again</v-btn>
 
-    <v-btn
-      flat
-      color="pink"
-    >
-      click me 
-    </v-btn>
+    <v-btn class="d-none d-lg-block">click me</v-btn>
 
-    <v-btn depressed class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>email: me</span>
-    </v-btn>
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>mdi-email</v-icon>
-      <span>email: me</span>
-    </v-btn>
-    <v-btn depressed large class="pink white--text">
-      <span>email: me</span>
-      <v-icon right large>mdi-email</v-icon>
-    </v-btn>
 
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>mdi-delete</v-icon>
-    </v-btn>
+    <v-btn class="d-md-block d-lg-none">click lg</v-btn>
+
+
+    <v-btn class="d-none d-sm-block">click sm</v-btn>
 
   </div>
 </template>
