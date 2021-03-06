748b83e5e5549f040c12511a9308326fa0167e5b
examp1 <bebe4kaxd@gmail.com>
2021-03-06 17:37:54 +0200

    theme setting

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,4 +8,16 @@ export default new Vuetify({
   icons: {
     iconfont: 'mdi', // default - only for display purposes
   },
+    //   натсройка тем https://vuetifyjs.com/en/features/theme/#light-and-dark
+  theme: {
+    themes: {
+      light: {
+        primary: '#9652ff',
+        secondary: '#b0bec5',
+        info: '#ffaa2c',
+        error: '#f83e70',
+        success: '#3cd1c2'
+      },
+    },
+  },
 })
