aa47f491b3ff57fe2a3dcfb53309a4b157a936d6
examp1 <bebe4kaxd@gmail.com>
2021-03-06 16:17:05 +0200

    commit 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
+   
+    <v-btn
+      class="pink white--text"
+    >click me</v-btn>
+    <v-btn
+      depressed
+      color="pink"
+    >click me again</v-btn>
+
+    <v-btn
+      flat
+      color="pink"
+    >
+      click me 
+    </v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>email: me</span>
+    </v-btn>
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>email: me</span>
+    </v-btn>
+    <v-btn depressed large class="pink white--text">
+      <span>email: me</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-delete</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
