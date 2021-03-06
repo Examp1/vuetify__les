0371eefa48f1b9939ff715e911ee3cd00648144e
examp1 <bebe4kaxd@gmail.com>
2021-03-06 17:58:20 +0200

    list and router

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -26,7 +26,20 @@
 
     <!-- проп temporary нужен для оверлея -->
     <v-navigation-drawer v-model="drawer" absolute temporary class="primary">
-        <p class="success">test</p>
+        <v-list>
+            <v-list-item v-for="(link, index) in links" :key="index" router :to="link.route">
+                <v-list-item-action>
+                    <v-icon class="white--text">
+                        {{ link.icon }}
+                    </v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title class="white--text">
+                        {{ link.text }}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
     </v-navigation-drawer>
 
   </nav>
@@ -36,7 +49,12 @@
 export default {
     data() {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                {icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/'},
+                {icon: 'mdi-folder', text: 'My Project', route: '/projects'},
+                {icon: 'mdi-account-supervisor', text: 'Team', route: '/team'},
+            ]
         }
     },
 };
